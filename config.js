1596ef51c44b098eb3997ff6843c629a04e90b22
Vishath456 <lakshanmd13@gmail.com>
2025-09-28 19:56:19 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 SESSION_ID: process.env.SESSION_ID || "",
-ALIVE_IMG: process.env.ALIVE_IMG || "https://github.com/DANUWA-MD/DANUWA-MD/blob/main/images/DANUWA-MD.png?raw=true",
+ALIVE_IMG: process.env.ALIVE_IMG || "https://github.com/Vishath456/Lakshanmd/commit/2d6206819309e969a9f0aaa8455e98286c609e14",
 ALIVE_MSG: process.env.ALIVE_MSG || "*Hello👋 LAKSHAN-MD Is Alive Now😍*",
 BOT_OWNER: '94765346615',  // Replace with the owner's phone number
 
