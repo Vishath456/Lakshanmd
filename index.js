1262a2a2edbf6ba5990caae2a001b7437b9dcbf3
Vishath456 <lakshanmd13@gmail.com>
2025-09-30 14:34:18 +0530

    Update index.js

--- index.js
+++ index.js
@@ -89,7 +89,7 @@ async function connectToWA() {
 
       const up = `DANUWA-MD connected ✅\n\nPREFIX: ${prefix}`;
       await danuwa.sendMessage(ownerNumber[0] + "@s.whatsapp.net", {
-        image: { url: `https://github.com/Vishath456/Lakshanmd/commit/2d6206819309e969a9f0aaa8455e98286c609e14` },
+        image: { url: `https://github.com/Vishath456/Lakshanmd/commit/cd67ef3e3b50e05d297b63fd77812ed95d3ba4bc` },
         caption: up
       });
 
