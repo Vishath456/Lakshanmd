8c2f21b62bfabdfda60758cad878635b0d740037
Vishath456 <lakshanmd13@gmail.com>
2025-10-20 12:46:59 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "4BEwTYxB#E0zntsfPlYd0IwrOhXS2k7I1_BEHASwj07ujoVxbCUU",
+SESSION_ID: process.env.SESSION_ID || "session id pest",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://github.com/Vishath456/Lakshanmd/commit/2d6206819309e969a9f0aaa8455e98286c609e14",
 ALIVE_MSG: process.env.ALIVE_MSG || "*Hello👋 LAKSHAN-MD Is Alive Now😍*",
 BOT_OWNER: '94765346615',  // Replace with the owner's phone number
